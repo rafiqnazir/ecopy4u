4f0b21ee104c49e409052cef0637e4759032335c
rafiqnazir <nademnazir79@gmail.com>
2020-12-01 14:54:09 +0530

    ENVIr

--- ecopy4u/settings.py
+++ ecopy4u/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '3n$c2)th4ot#k2q2u^!9pq2=nwd-q7e3oia)c-pve$_%g7g#y#'
 
-#SECRET_KEY=os.environ.get('SECRET_KEY')
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
