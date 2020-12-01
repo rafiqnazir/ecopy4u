70b0279f32621c5f2880a3c3d0d1f9e0581f80b1
rafiqnazir <nademnazir79@gmail.com>
2020-12-01 15:57:03 +0530

    Models

--- ecopy4u/settings.py
+++ ecopy4u/settings.py
@@ -162,5 +162,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL=None
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-2'
+
 
 django_heroku.settings(locals())
