0c799a49c92e704fbf00bf321cb5e1756d993ff7
rafiqnazir <nademnazir79@gmail.com>
2020-12-02 12:19:31 +0530

    tru

--- ecopy4u/settings.py
+++ ecopy4u/settings.py
@@ -84,16 +84,18 @@ WSGI_APPLICATION = 'ecopy4u.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
 "default": {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 }
 }
+'''
 DATABASES={
     "default":dj_database_url.config()
 }
+
 '''
 import dj_database_url
 DATABASES = {
