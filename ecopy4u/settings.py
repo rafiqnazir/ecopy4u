c37492df054af3897bc941bed2e043738bd49a65
rafiqnazir <nademnazir79@gmail.com>
2020-12-03 12:52:41 +0530

    git

--- ecopy4u/settings.py
+++ ecopy4u/settings.py
@@ -188,11 +188,6 @@ AWS_S3_REGION_NAME = 'us-east-2'
 django_heroku.settings(locals())
 #import dj_database_url
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)'''
-if ON_HEROKU:
-    DATABASE_URL = 'postgresql://<postgresql>'
-else:
-    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-#DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
