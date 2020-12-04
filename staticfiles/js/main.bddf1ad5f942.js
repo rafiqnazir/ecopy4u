8ee52b1f23b69336a8751a3d80d05ff5650e6499
rafiqnazir <nademnazir79@gmail.com>
2020-12-04 15:22:45 +0530

    new

---
+++ staticfiles/js/main.bddf1ad5f942.js
@@ -0,0 +1,10 @@
+$('.count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
