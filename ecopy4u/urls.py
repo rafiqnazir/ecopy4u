f04c1decac9a5b347e07de4b8d94c73f081f7375
rafiqnazir <nademnazir79@gmail.com>
2020-12-04 12:26:54 +0530

    url

--- ecopy4u/urls.py
+++ ecopy4u/urls.py
@@ -21,20 +21,20 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecopy4u/',include('home.urls')),
-    path('ecopy4u/boards/',include('boards.urls')),
-    path('ecopy4u/colleges/',include('colleges.urls')),
-    path('ecopy4u/register/',user_views.register ,name="register"),
-    path('ecopy4u/login/',auth_views.LoginView.as_view( template_name='users/login.html' ) ,name="login"),
-    path('ecopy4u/logout/',auth_views.LogoutView.as_view( template_name='users/logout.html' ) ,name="logout"),
-    path('ecopy4u/profile/',user_views.profile ,name="profile"),
-    path('ecopy4u/password-reset/',auth_views.PasswordResetView.as_view(template_name=
+    path('',include('home.urls')),
+    path('boards/',include('boards.urls')),
+    path('colleges/',include('colleges.urls')),
+    path('register/',user_views.register ,name="register"),
+    path('login/',auth_views.LoginView.as_view( template_name='users/login.html' ) ,name="login"),
+    path('logout/',auth_views.LogoutView.as_view( template_name='users/logout.html' ) ,name="logout"),
+    path('profile/',user_views.profile ,name="profile"),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name=
         'users/password_reset.html' ) ,name="password_reset"),
-    path('ecopy4u/password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name=
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name=
         'users/password_reset_done.html' ) ,name="password_reset_done"),
-    path('ecopy4u/password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name=
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name=
         'users/password_reset_confirm.html' ) ,name="password_reset_confirm"),
-    path('ecopy4u/password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name=
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name=
         'users/password_reset_complete.html' ) ,name="password_reset_complete"),
 
 
