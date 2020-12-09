9516eef3050cad19d82b581178d64b7b31575256
rafiqnazir <nademnazir79@gmail.com>
2020-12-09 12:39:27 +0530

    aws_en

--- ecopy4u/settings.py
+++ ecopy4u/settings.py
@@ -172,17 +172,16 @@ EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-if (os.environ.get('DEBUG_VALUE') == 'False'):
-    AWS_ACCESS_KEY_ID =os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID =os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL=None
-    DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL=None
+DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 
-    AWS_S3_SIGNATURE_VERSION = 's3v4'
-    AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-2'
 
 
 django_heroku.settings(locals())
