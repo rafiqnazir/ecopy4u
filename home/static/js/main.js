b51314bdde9088e116666d1bc0d2fdf2521d765a
rafiqnazir <nademnazir79@gmail.com>
2020-12-31 22:32:10 +0530

    Spinner Decreased

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -3,7 +3,7 @@ $('.count').each(function () {
   $(this).prop('Counter',0).animate({
       Counter: $(this).text()
   }, {
-      duration: 5000,
+      duration: 2000,
       easing: 'swing',
       step: function (now) {
           $(this).text(Math.ceil(now));
